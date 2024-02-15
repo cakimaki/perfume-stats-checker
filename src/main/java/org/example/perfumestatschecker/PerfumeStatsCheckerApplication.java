0fa72e292ccf59f987028f9db2f90d1b3adaaa8a
cakimaki <dani.tsakovski3@gmail.com>
2024-02-15 23:21:55 +0200

    Project Created

---
+++ src/main/java/org/example/perfumestatschecker/PerfumeStatsCheckerApplication.java
@@ -0,0 +1,13 @@
+package org.example.perfumestatschecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumeStatsCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfumeStatsCheckerApplication.class, args);
+	}
+
+}
